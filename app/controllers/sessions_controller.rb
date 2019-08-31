8b54273dfb1cc3d7a3491ab26b1d4a73cdefe77c
erib <eri-b@users.noreply.github.com>
2019-08-31 10:06:36 -0400

    small updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,9 @@
 class SessionsController < ApplicationController
   def create
 
-    #render 'sites/temp'
     site = Site.find_by(name: params[:session][:site])
     if site && site.authenticate(params[:session][:password])
-      session[site.name.to_sym] = "unlocked"
+      session[site.name.to_sym] = "session-unlocked"
       redirect_to main_path(site.name), notice: 'Temporarily unlocked'
     else
       flash.now[:danger] = 'Invalid password'
