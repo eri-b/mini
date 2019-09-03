159ee2f42220fdf89818b03ffbb44bacfd337202
erib <eri-b@users.noreply.github.com>
2019-09-02 20:35:30 -0400

    resolved issue with migration using reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_154937) do
+ActiveRecord::Schema.define(version: 2019_09_02_234118) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2019_08_20_154937) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "locked"
-    t.datetime "last_post"
   end
 
 end
